88048eb7088217a68c35717749e096aa336146d5
Ronald <ralvarez@pucp.edu.pe>
2019-08-21 19:57:07 -0500

    primer commit

---
+++ app/src/main/java/pe/edu/pucp/perugoproy/presentation/evento/view/MainActivity.java
@@ -0,0 +1,16 @@
+package pe.edu.pucp.perugoproy.presentation.evento.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pe.edu.pucp.perugoproy.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
