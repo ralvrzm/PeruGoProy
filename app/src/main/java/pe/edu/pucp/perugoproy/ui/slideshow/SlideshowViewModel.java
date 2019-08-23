a364c90fd303cd92e059d3b8ea45e5c77777e7cf
Ronald <ralvarez@pucp.edu.pe>
2019-08-23 16:56:45 -0500

    barra lateral

---
+++ app/src/main/java/pe/edu/pucp/perugoproy/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package pe.edu.pucp.perugoproy.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
